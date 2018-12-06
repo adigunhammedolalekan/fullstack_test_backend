7d480f9720019ffc71f787579ede5d30364a1067
Victor Akerele <eneroakerele@gmail.com>
2018-12-06 21:56:27 +0100

    added cors

--- src/router.js
+++ src/router.js
@@ -1,23 +1,6 @@
 const router = require('express').Router();
-const Joi = require('joi');
-const UserValidator = require('./User/UserValidator');
-const { verifyEmail, verifyPassword } = require('./User/UserData');
-const generateToken = require('./token');
+const LoginController = require('./controllers/LoginController');
 
-router.post('/login', (req, res) => {
-	const data = ({ email, password } = req.body);
-	Joi.validate(data, UserValidator)
-		.then(() => verifyEmail(data))
-		.then(result => verifyPassword(result))
-		.then(result => generateToken(result))
-		.then(result => res.status(200).json({ body: result, error: false, message: 'Login successful' }))
-		.catch(err => {
-			if (err.isJoi) {
-				res.status(422).json({ body: err, error: true, message: 'Validation error' });
-			} else {
-				res.status(500).json({ body: `${err}`, error: true, message: 'Error in logging in user' });
-			}
-		});
-});
+router.post('/login', LoginController);
 
 module.exports = router;
